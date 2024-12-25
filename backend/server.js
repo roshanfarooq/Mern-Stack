934af0c0176fe49169e371f6769fffbbfc4020ad
roshanfarooq <roshanfarooq786@gmail.com>
2024-12-25 22:26:37 +0500

    4th-commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors=require('cors')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
